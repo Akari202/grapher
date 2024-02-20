adc04b87019115398355adebb7e2988422b9f99f
Akari <akaritwo0two@gmail.com>
2024-02-20 11:27:56 -0500

    Add Insertion sort and sorting framework

--- src/config.rs
+++ src/config.rs
@@ -9,5 +9,6 @@ pub(crate) const WINDOW_WIDTH: u32 = 700;
 pub(crate) const CENTER_X: u32 = WINDOW_WIDTH / 2;
 pub(crate) const CENTER_Y: u32 = WINDOW_HEIGHT / 2;
 pub(crate) const TICKS_PER_SECOND: u32 = 20;
-pub(crate) const FRAMERATE: u32 = 60;
+pub(crate) const FRAMERATE: u32 = 20;
+pub(crate) const PLAY_AUDIO: bool = false;
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+#![feature(is_sorted)]
 extern crate sdl2;
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use std::time::Duration;
+use crate::axis::Axis2D;
 use crate::colors::{BLUE, GREEN, RED, WHITE};
 use crate::config::{CENTER_Y, WINDOW_HEIGHT, WINDOW_WIDTH};
 use crate::coordinate::PixelCoordinate2D;
@@ -18,6 +20,8 @@ mod colors;
 mod function;
 mod scatter;
 mod cursor;
+mod sort;
+mod audio;
 
 pub fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
@@ -37,42 +41,42 @@ pub fn main() -> Result<(), String> {
 
     let mut renderer = renderer::Renderer::new(window, sdl_context)?;
 
-    renderer.draw_rect(
-        PixelCoordinate2D::origin(),
-        WINDOW_WIDTH,
-        WINDOW_HEIGHT,
-        WHITE,
-    )?;
+    let mut axis: Axis2D;
+    {
+        axis = axis::Axis2D::new(
+            PixelCoordinate2D::new(config::CENTER_X as i32, config::CENTER_Y as i32),
+            WINDOW_WIDTH as f32 / (4.0 * std::f32::consts::PI),
+            WINDOW_HEIGHT as f32 / 2.5,
+            0.01
+        );
+        axis.add_object(Box::new(cursor::SnappingCursor::new(&renderer.sdl)));
+        axis.add_object(Box::new(cursor::CursorReadout::new(PixelCoordinate2D::new(WINDOW_WIDTH as i32 - 175, 0), ReadoutType::Cartesian)));
+        axis.add_object(Box::new(cursor::CursorReadout::new(PixelCoordinate2D::new(WINDOW_WIDTH as i32 - 175, 25), ReadoutType::Pixel)));
 
-    let mut axis = axis::Axis2D::new(
-        PixelCoordinate2D::new(config::CENTER_X as i32, config::CENTER_Y as i32),
-        WINDOW_WIDTH as f32 / (4.0 * std::f32::consts::PI),
-        WINDOW_HEIGHT as f32 / 2.5,
-        0.01
-    );
-    axis.add_object(Box::new(cursor::SnappingCursor::new(&renderer.sdl)));
-    axis.add_object(Box::new(cursor::CursorReadout::new(PixelCoordinate2D::new(WINDOW_WIDTH as i32 - 175, 0), ReadoutType::Cartesian)));
-    axis.add_object(Box::new(cursor::CursorReadout::new(PixelCoordinate2D::new(WINDOW_WIDTH as i32 - 175, 25), ReadoutType::Pixel)));
-
-    axis.add_object(Box::new(UnaryFunction::new(Box::new(|x| x.sin()), BLUE)));
-    axis.add_object(Box::new(UnaryFunction::new(Box::new(
-        |x| {
-            4.0 / std::f32::consts::PI * (0..3).fold(0.0, |acc, n| {
-                let n = 2 * n + 1;
-                acc + (1.0 / n as f32) * (n as f32 * x).sin()
-            })
-        }
-    ), GREEN)));
-    axis.add_object(Box::new(UnaryFunction::new(Box::new(
-        |x| {
-            if x.sin() > 0.0 {
-                1.0
-            } else {
-                -1.0
+        axis.add_object(Box::new(UnaryFunction::new(Box::new(|x| x.sin()), BLUE)));
+        axis.add_object(Box::new(UnaryFunction::new(Box::new(
+            |x| {
+                4.0 / std::f32::consts::PI * (0..3).fold(0.0, |acc, n| {
+                    let n = 2 * n + 1;
+                    acc + (1.0 / n as f32) * (n as f32 * x).sin()
+                })
             }
-        }
-    ), RED)));
-    // axis.add_object(Box::new(UnaryFunction::new(Box::new(|x| x.powi(5) - 10.0 * x.powi(4) + 40.0 * x.powi(3) - 80.0 * x.powi(2) + 80.0 * x - 32.0))));
+        ), GREEN)));
+        axis.add_object(Box::new(UnaryFunction::new(Box::new(
+            |x| {
+                if x.sin() > 0.0 {
+                    1.0
+                } else {
+                    -1.0
+                }
+            }
+        ), RED)));
+        // axis.add_object(Box::new(UnaryFunction::new(Box::new(|x| x.powi(5) - 10.0 * x.powi(4) + 40.0 * x.powi(3) - 80.0 * x.powi(2) + 80.0 * x - 32.0))));
+    }
+
+
+    // let mut sort_vis = sort::SortingVisualization::new(sort::SortType::Shell);
+    let mut sort_vis = sort::SortingVisualization::new(sort::SortType::Insertion);
 
     'running: loop {
         for event in renderer.event_pump.poll_iter() {
@@ -82,16 +86,33 @@ pub fn main() -> Result<(), String> {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'running,
+                Event::KeyDown {
+                    keycode: Some(Keycode::S),
+                    ..
+                } => { sort_vis.shuffle() },
+                Event::KeyDown {
+                    keycode: Some(Keycode::Space),
+                    ..
+                } => { if !sort_vis.sorted { sort_vis.step() }},
+                Event::KeyDown {
+                    keycode: Some(Keycode::A),
+                    ..
+                } => { if !sort_vis.sorted { sort_vis.auto_sort = !sort_vis.auto_sort }},
                 _ => {}
             }
         }
 
+        if sort_vis.auto_sort {
+            sort_vis.step();
+        }
+
         renderer.clear();
-        renderer.draw_object(&axis)?;
+        // renderer.draw_object(&axis)?;
+        renderer.draw_object(&sort_vis)?;
 
         renderer.present();
 
-        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / config::FRAMERATE));
+        std::thread::sleep(Duration::new(0, 1000000000 / config::FRAMERATE));
     }
 
     Ok(())
